c2a794763059d02144eac4dec9fc4dd7aefebaae
Jesus Angel Hernandez de Rojas <jesusangel.hernandez@uva.es>
2016-04-19 14:02:28 +0200

    Subida del otro prototipo

--- prototipo2/js/principal.js
+++ prototipo2/js/principal.js
@@ -4,6 +4,15 @@ $(document).ready(function(){
 	setTimeout(function() {
 		cerrar_aviso();
 	}, 20000);
+
+	$(".nav_tlf").on("change", function(){
+		var url =$(this).val();
+		if (url) {
+			window.location = url;
+		}
+		return false;
+	});
+
 });
 
 function cerrar_aviso() {
